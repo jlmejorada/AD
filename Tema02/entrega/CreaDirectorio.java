24779c71fd49e363044657a8dfbdb1b229a6fea5
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-10-18 10:05:26 +0200

    Subida crear directorio

---
+++ Tema02/entrega/CreaDirectorio.java
@@ -0,0 +1,48 @@
+package directorios;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CreaDirectorio {
+
+	public static void main(String[] args) {
+
+		String directorio = "C:\\Users\\jlmejorada\\";
+
+		leeDocumento(directorio);
+
+	}
+
+	public static void leeDocumento(String directorio) {
+		String rutaArchivo = "src/archivo/carpetas.txt";
+		String line = "";
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(rutaArchivo))) {
+
+			while ((line = reader.readLine()) != null) {
+
+				if (creaDirectorio(directorio, line)) {
+					System.out.println("Directorio " + directorio + line + " creado correctamente");
+				} else {
+					System.out.println("La carpeta ya existe");
+				}
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static boolean creaDirectorio(String directorio, String linea) {
+
+		return new File(directorio + linea).mkdir();
+
+	}
+
+}
